ef26a5f6959f548840cf7ae387bceca2b7b17fee
Phan Vĩnh Khang <khangpv.19th@sv.dla.edu.vn>
2023-11-22 16:02:55 +0700

    m

--- src/migrations/20231122083239-create-user-2.js
+++ src/migrations/20231122083239-create-user-2.js
@@ -2,30 +2,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', { //create table
+    await queryInterface.createTable('User2s', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
         type: Sequelize.STRING
       },
       email: {
         type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      avatar: {
-        type: Sequelize.STRING
-      },
-      adress: {
-        type: Sequelize.STRING
-      },
-      roleid: {
-        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
@@ -38,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('User2s');
   }
 };
--- src/model/database.js
+++ src/model/database.js
@@ -7,10 +7,16 @@ const db = mysql.createPool({
     user: 'root',
     database: 'testdbNodeJS'
 });
-const connect_db = new Sequelize('testdbstore', 'root', undefined, {
-    host: 'localhost',
+// Host: sql12.freesqldatabase.com
+// Database name: sql12664061
+// Database user: sql12664061
+// Database password: M7Z2kFV5XP
+// Port number: 3306
+const connect_db = new Sequelize('sql12664174', 'sql12664174', '8v71UFIVEW', {
+    host: 'sql12.freesqldatabase.com',
     dialect: 'mysql',
     logging: false,
+    port: 3306
 });
 const check_connect = async (sequelize) => {
     try {
